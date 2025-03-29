93abf593a7e2dc12fcac09bb7c7bf51ba8d9e484
yeekom <yeekom@proton.me>
2025-03-29 13:57:53 +1300

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,11 +21,28 @@ def run_web():
 @bot.event
 async def on_ready():
     await tree.sync()
-    print(f"Bot is ready! Logged in as {bot.user}")
 
+@discord.app_commands.allowed_installs(guilds=False, users=True)
+@discord.app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True)
 @tree.command(name="hello", description="Replies with a hello message!")
 async def hello(interaction: discord.Interaction):
-    await interaction.response.send_message("Hello! I'm alive and running on Render!")
+    await interaction.response.send_message("pong", ephemeral=True)
+    await interaction.user.send("pong")
 
-threading.Thread(target=run_web).start()  # Start web server in a separate thread
+@bot.event
+ async def on_message(message):
+     if message.author == bot.user:
+         return
+     if isinstance(message.channel, discord.DMChannel):
+         if message.content.isdigit():
+             user_id = int(message.content)
+             try:
+                 user = await bot.fetch_user(user_id)
+ 
+                 pfp_url = user.avatar.url  
+                 response = f"{pfp_url}"
+ 
+                 await message.channel.send(response)
+
+threading.Thread(target=run_web).start() 
 bot.run(TOKEN)
